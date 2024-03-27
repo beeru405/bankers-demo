ec0992930b613966e68ce42154bf92f624ef9614
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-03-27 14:41:29 +0530

    Update CalculatorUITest.java

--- src/test/java/com/example/CalculatorUITest.java
+++ src/test/java/com/example/CalculatorUITest.java
@@ -1,3 +1,7 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
